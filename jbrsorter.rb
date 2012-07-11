6abe29a492ff25a7718f29d3e1f696ca202b094f
Jon Rubin <jb.rubin@gmail.com>
2012-07-10 20:01:25 -0400

    Comments about functionality to guide my coding.

--- jbrsorter.rb
+++ jbrsorter.rb
@@ -1,2 +1,8 @@
 #! /usr/bin/ruby
 
+# Read in a large file
+# (To test, I need to generate 1 trillion random int32_ts and write those to a test file).
+
+# Sort the numbers
+
+# Write the lowest N numbers to a file
