ce5964899735cd29cf78f90e2dbe91fda32f61cd
Jon Rubin <jb.rubin@gmail.com>
2012-07-10 20:13:36 -0400

    How *do* you tell Ruby to do something 1 trillion times? One sec while I look it up instead of trying to do something clever like representing exponents with nested loops.

--- jbrsorter.rb
+++ jbrsorter.rb
@@ -1,7 +1,15 @@
 #! /usr/bin/ruby
 
 # Read in a large file
+
 # (To test, I need to generate 1 trillion random int32_ts and write those to a test file).
+def generateTestFile( filename )
+  File.new(filename, "w")
+  .times do # Huh I guess I need to look up how to do scientific notation in Ruby...
+    
+  end
+end
+
 
 # Sort the numbers
 
