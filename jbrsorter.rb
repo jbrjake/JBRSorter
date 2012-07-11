afa526f6ad8182723c7c00bd7293d5eb7e6ce504
Jon Rubin <jb.rubin@gmail.com>
2012-07-10 20:17:50 -0400

    Fleshes out the test file generator.

--- jbrsorter.rb
+++ jbrsorter.rb
@@ -5,12 +5,16 @@
 # (To test, I need to generate 1 trillion random int32_ts and write those to a test file).
 def generateTestFile( filename )
   File.new(filename, "w")
+  file = File.open(filename, "w")
   100.times do # Okay for expediency's sake I'll use a set of 100
-    
+    file.puts rand().to_s
   end
+  file.close
 end
 
 
 # Sort the numbers
 
 # Write the lowest N numbers to a file
+
+generateTestFile( "testfile.txt" )
