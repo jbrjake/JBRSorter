975cc74f29e90af847851035784ab662d9bc0768
Jon Rubin <jb.rubin@gmail.com>
2012-07-10 20:35:04 -0400

    Crude method to get at an entry from the testfile.

--- jbrsorter.rb
+++ jbrsorter.rb
@@ -1,7 +1,5 @@
 #! /usr/bin/ruby
 
-# Read in a large file
-
 # (To test, I need to generate 1 trillion random int32_ts and write those to a test file).
 # This is outputting floats, instead, but the principle's the same so I'll come back to this later
 def generateTestFile( filename )
@@ -13,9 +11,24 @@ def generateTestFile( filename )
   file.close
 end
 
+# Read in a large file
+# Okay this pass at it would not work for large files (seek time would be ridiculous), but it lets me get to the fun part, sorting.
+# Eventually I see it working by chunking up the file load by a certain number of lines, and pre-loading them into an array
+# Then it would only have to read the file every "certain number of lines".
+def loadNumberFromFile( filename, n )
+  file = File.open(filename, "r")
+  while file.lineno < n - 1
+    file.gets
+  end 
+  theNum = file.gets
+  file.close
+  return theNum
+end
 
 # Sort the numbers
 
 # Write the lowest N numbers to a file
 
-generateTestFile( "testfile.txt" )
+# generateTestFile( "testfile.txt" )
+
+puts loadNumberFromFile( "testfile.txt", 5 )
